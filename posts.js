5d760c5a7f8da7fbb1c00982b2ba610d95430194
umeshsobti <umesh@trigvent.com>
2024-07-01 15:58:45 +0530

    Redux thunk, api fetch

--- posts.js
+++ posts.js
@@ -1,43 +1,77 @@
-const {createStore, applyMiddleware} = require('redux')
-const loggerMiddleware = require('redux-logger').createLogger()
+const { createStore, applyMiddleware } = require("redux");
+const thunk = require("redux-thunk").default;
+const axios = require("axios");
+//custom Middleware
+
+// const logger = () => {
+//   return (next) => {
+//     return (action) => {
+//       console.log("action fired", action);
+//       next(action);
+//     };
+//   };
+// };
 //initialState
 
 const initialState = {
-    posts:[],
-}
+  posts: [],
+  error: "",
+  loading: false,
+};
 
-const fetchPostRequest = ()=>{
-    return{
-        type: "REQUEST_STARTED",
-    };
-}
+const FETCH_SUCCESS = "FETCH_SUCCESS";
+const REQUEST_STARTED = "REQUEST_STARTED";
+const FETCH_FAILED = "FETCH_FAILED";
 
-const fetchPostSuccess = ()=>{
-    return{
-        type: "FETCH_SUCCESS",
-    };
-}
+const fetchPostRequest = () => {
+  return {
+    type: REQUEST_STARTED,
+  };
+};
 
-const fetchPostFaliure = ()=>{
-    return{
-        type: "FETCH_FAILED",
-    };
-}
+const fetchPostSuccess = (posts) => {
+  return {
+    type: FETCH_SUCCESS,
+    payload: posts,
+  };
+};
 
-const postsReducer=(state=initialState,action)=>{
-    switch(action.type){
-        case "REQUEST_STARTED":
-            return{
-                posts: ["HTML"],
-            }; 
+const fetchPostFaliure = (err) => {
+  return {
+    type: FETCH_FAILED,
+    payload: err,
+  };
+};
+
+const fetchPosts = () => {
+  return async (dispatch) => {
+    try {
+      dispatch(fetchPostRequest());
+      const data = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      dispatch(fetchPostSuccess(data));
+    } catch (error) {
+      dispatch(fetchPostFaliure(error.message));
     }
-}
+  };
+};
 
-const store = createStore(postsReducer,applyMiddleware(loggerMiddleware))
+const postsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case REQUEST_STARTED:
+      return {
+        ...state,
+        loading: true,
+      };
+  }
+};
 
-store.subscribe(()=>{
-    const data = store.getState();
-    console.log(data)
+const store = createStore(postsReducer, applyMiddleware(thunk));
+
+store.subscribe(() => {
+  const data = store.getState();
+  console.log(data);
 });
 
-store.dispatch(fetchPostRequest());
+store.dispatch(fetchPosts());
