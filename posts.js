e56ca39f8f5d9d627b19a9c33e26262cf5d996b4
umeshsobti <umesh@trigvent.com>
2024-07-01 13:00:33 +0530

    added redux-logger middleware

--- posts.js
+++ posts.js
@@ -1,5 +1,43 @@
+const {createStore, applyMiddleware} = require('redux')
+const loggerMiddleware = require('redux-logger').createLogger()
 //initialState
 
 const initialState = {
     posts:[],
 }
+
+const fetchPostRequest = ()=>{
+    return{
+        type: "REQUEST_STARTED",
+    };
+}
+
+const fetchPostSuccess = ()=>{
+    return{
+        type: "FETCH_SUCCESS",
+    };
+}
+
+const fetchPostFaliure = ()=>{
+    return{
+        type: "FETCH_FAILED",
+    };
+}
+
+const postsReducer=(state=initialState,action)=>{
+    switch(action.type){
+        case "REQUEST_STARTED":
+            return{
+                posts: ["HTML"],
+            }; 
+    }
+}
+
+const store = createStore(postsReducer,applyMiddleware(loggerMiddleware))
+
+store.subscribe(()=>{
+    const data = store.getState();
+    console.log(data)
+});
+
+store.dispatch(fetchPostRequest());
